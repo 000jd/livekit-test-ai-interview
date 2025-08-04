32bd824b8230810140e26855d9739a82bafd1363
000jd <joydipd793@gmail.com>
2025-08-04 15:49:22 +0530

    this vertion works

--- agent.py
+++ agent.py
@@ -273,7 +273,7 @@ async def entrypoint(ctx: agents.JobContext):
         tts = cartesia.TTS(
             api_key=os.getenv("CARTESIA_API_KEY"),
             model="sonic-2",
-            voice="794f9389‑aac1‑45b6‑b726‑9d9369183238",
+            voice="794f9389-aac1-45b6-b726-9d9369183238",
             #sample_rate=24000,
             #language="en",
             #encoding="pcm_s16le",
